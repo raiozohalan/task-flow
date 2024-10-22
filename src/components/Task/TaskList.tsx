1d96c376d82d99ef2de4119c2c31369d9146c627
Joelhy Galan <raizohgalan@gmail.com>
2024-10-22 13:44:43 +0800

    refactor: TaskList component to remove dummy data and unused imports

--- src/components/Task/TaskList.tsx
+++ src/components/Task/TaskList.tsx
@@ -1,6 +1,6 @@
 import { CheckSquare, Grid, List } from "react-feather";
 import classNames from "../../utils/classNames";
-import { Task, TaskStatuses } from "./interface.d";
+import { Task } from "./interface.d";
 import TaskItem from "./TaskItem";
 import { useEffect, useMemo, useState } from "react";
 import { useAppSelector } from "../../redux/hook";
@@ -11,36 +11,6 @@ export interface TaskListProps {
   data?: Task[];
   display?: TaskDisplay;
 }
-
-const LOREM_IPSUM = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
-
-const DUMMY_DATA: Task[] = [
-  {
-    id: "task-1",
-    title: "Task 1",
-    description: LOREM_IPSUM,
-    status: TaskStatuses.PENDING,
-    due_date: new Date(),
-  },
-  {
-    id: "task-2",
-    title: "Task 2",
-    description: LOREM_IPSUM,
-    status: TaskStatuses.PENDING,
-    due_date: new Date(),
-  },
-  {
-    id: "task-3",
-    title: "Task 3",
-    description: LOREM_IPSUM,
-    status: TaskStatuses.PENDING,
-    due_date: new Date(),
-  },
-];
 
 const TASK_DISPLAY = {
   list: {
